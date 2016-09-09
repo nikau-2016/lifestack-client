377ad7ed8bc2c51083412eee429f4ca93e50f4b6
Kamon-Harrell <harrellkamon@gmail.com>
2016-09-09 18:19:46 +1200

    added buttons to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,27 @@ export default (props) => {
   return (
     <div>
       <header>
-        <p>Placeholder for Header</p>
-        <Link to={'/'}>Home</Link>
-        <Link to={'/profile'}>Profile</Link>
-        <Link to={'/search'}>Search</Link>
+      <hr />
+        <h1>#ADULTING</h1>
+          <hr />
+          <Link to={'/'}>Home</Link>
+          <hr />
+          <Link to={'/profile'}>Profile</Link>
+          <hr />
+          <Link to={'/search'}>Search</Link>
+          <hr />
+          <form>
+            <input type="text" className="username" placeholder="UserName" />
+              <hr />
+              <input type="text" className="password" placeholder="Password" />
+                <hr />
+              <button className="login-btn" type="button">Login</button>
+            </form>
+            <hr />
+            <button className="PLACEHOLDER-FOR-OAUTH-BUTTON" type="button">Register</button>
+            <button className="skills-btn">Skills</button>
+            <button className="catagories-btn">Catagories</button>
+            <hr />
       </header>
       <div>
         {props.children}
