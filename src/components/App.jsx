574ede4fc5d4d501d057cfc66db50c132249e058
Kamon-Harrell <harrellkamon@gmail.com>
2016-09-09 15:02:47 +1200

    added profile component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import SkillVideo from './SkillVideo'
+import Profile from './Profile'
 
 export default () => {
-  return <SkillVideo id={6} url="https://www.youtube.com/embed/1w-oQ-i1XB8" votes={320} onUpvote={evt => console.log(evt.target.id)} onDownvote={evt => console.log(evt.target.id)} />
+  return <Profile id={6} userName="Kerbonaut" profilePic="http://i.imgur.com/oPyO3CJ.png" />
 }
