be7b0cc1a055f7bd60aee70d3a4afb6222ad3104
Justin <justindado@yahoo.com>
2016-09-09 15:14:25 +1200

    added UserSkill component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import SkillVideo from './SkillVideo'
+import UserSkill from './UserSkill'
 
 export default () => {
-  return <SkillVideo id={6} url="https://www.youtube.com/embed/1w-oQ-i1XB8" votes={320} onUpvote={evt => console.log(evt.target.id)} onDownvote={evt => console.log(evt.target.id)} />
+  return(
+    <div>
+      <SkillVideo id={6} url="https://www.youtube.com/embed/1w-oQ-i1XB8" votes={320} onUpvote={evt => console.log(evt.target.id)} onDownvote={evt => console.log(evt.target.id)} />
+      <UserSkill id={1} skillName="Fry Egg" status="watched" skillXp={25} showcase="" />
+    </div>
+  )
 }
--- src/components/UserSkill.jsx
+++ src/components/UserSkill.jsx
@@ -5,21 +5,17 @@ export default React.createClass({
     skillName: React.PropTypes.string.isRequired,
     status: React.PropTypes.string.isRequired,
     skillXp: React.PropTypes.number.isRequired,
-    showcase: React.PropTypes.string.isRequired
+    showcase: React.PropTypes.string
   },
 
   render() {
     return(
-      <div className="user-skill">
-        <table>
           <tr>
             <th>{this.props.skillName}</th>
             <td>{this.props.status}</td>
             <td>{this.props.skillXp}</td>
             <td>{this.props.showcase}</td>
           </tr>
-        </table>
-      </div>
     )
   }
 })
