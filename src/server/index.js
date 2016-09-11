9be0509be62fc37664cfe8007fd88dc7c53db5a9
Sach <shsaci@yahoo.com.au>
2016-09-11 16:48:21 +1200

    Add basic server files

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,12 @@
-// var express = require('express')
-// const cors = require('cors')
-//
-// var app = express()
-// var PORT = process.env.PORT || 5000
-//
-// app.use(express.static(__dirname + './../public'))
-// app.use(cors())
-//
-// app.listen(PORT, function () {
-//   console.log('Listening on port ' + PORT)
-// })
+var express = require('express')
+const cors = require('cors')
+
+var app = express()
+var PORT = process.env.PORT || 5000
+
+app.use(express.static(__dirname + './../public'))
+app.use(cors())
+
+app.listen(PORT, function () {
+  console.log('Listening on port ' + PORT)
+})
